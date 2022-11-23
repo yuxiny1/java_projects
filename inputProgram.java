32f01d2fdaed25df2981038b7abf14b6d73a23ca
yuxiny1 <yuxiny1@me.com>
2022-11-23 13:46:20 +0000

    update

---
+++ inputProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class inputProgram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter your first name :");
+        String firstName = input.next();
+
+        System.out.print("enter your last name: ");
+        String lastName = input.next();
+
+        System.out.println("Your full name is " + firstName + " " + lastName);
+
+    }
+}
