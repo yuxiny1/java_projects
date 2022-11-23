b1f4a081117ea4e208bc5054dd412ce009ba57c8
yuxiny1 <yuxiny1@me.com>
2022-11-23 14:49:12 +0000

    Update inputProgram.java

--- inputProgram.java
+++ inputProgram.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 class inputProgram {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
+        Scanner input= new Scanner(System.in);
+        
         System.out.print("enter your first name :");
         String firstName = input.next();
 
