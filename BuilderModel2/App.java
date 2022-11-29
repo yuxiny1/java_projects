6a90fde4d7ff9eeee86d123b7f1d75368a754bcb
yuxiny1 <yuxiny1@me.com>
2022-11-29 19:38:06 +0000

    update
    update

--- BuilderModel2/App.java
+++ BuilderModel2/App.java
@@ -28,5 +28,6 @@ public class App {
                     .build();
 
         System.out.println(mySUV.toString());
+        
     }
 }
--- SingleTonDesignPattern/PerformanceStage.java
+++ SingleTonDesignPattern/PerformanceStage.java
@@ -9,7 +9,8 @@ public class PerformanceStage{
     }
     // instasnce , static, becasue we want it to be sort of an instance independent field. value stored in the static variable is shared by all instances of the class, only one instance of the class can be created
     
-        
+    
+    public static PerformanceStage getInstance(){
         if(INSTANCE == null){
             INSTANCE = new PerformanceStage();
         }
